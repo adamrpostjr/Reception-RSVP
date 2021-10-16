b4772de3ab22314ffdb920db8e6c6684360a34b2
Adam <adamrpostjr@gmail.com>
2021-10-15 22:32:49 -0400

    shutting down rsvp

--- src/Card.svelte
+++ src/Card.svelte
@@ -3,7 +3,6 @@
 
 <card>
   <img id="card" src="/Reception.webp" alt="" />
-  <scroll> &#8659; &#8659; &#8659; </scroll>
 </card>
 
 <style>
@@ -14,50 +13,20 @@
     height: 100vh;
     border-radius: 0 25px 25px 0;
   }
-  scroll {
-    display: none;
-  }
   @media only screen and (max-width: 768px) {
     card {
-      /* position: absolute; */
       width: 100vw;
-      height: 100vh;
-      float: unset;
-      order: 1;
+      height: 100%;
+      text-align: center;
     }
     #card {
-      width: 100vw;
-      height: 100vh;
     }
-    scroll {
-      display: block;
-      position: absolute;
-      bottom: 10px;
-      left: 50%;
-      font-family: monospace;
-      font-size: 20px;
-      animation: updown 3s ease infinite;
-    }
-    @keyframes updown {
-      0% {
-        transform: translateY(-50%) translate(-50%);
-      }
-
-      50% {
-        transform: translateY(50%) translate(-50%);
-      }
-
-      100% {
-        transform: translateY(-50%) translate(-50%);
-      }
+    h1 {
+      text-align: center;
     }
   }
 
   @media only screen and (max-width: 414px) {
-    #card {
-      width: 100%;
-      height: unset;
-    }
     card {
       justify-content: center;
       display: flex;
